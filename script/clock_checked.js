55891e8c0092d186184bbb28127e102496d070cf
Nguyễn Bùi Nhựt Ý <130396826+NguyenNhutY@users.noreply.github.com>
2023-10-02 17:37:48 +0700

    1

--- script/clock_checked.js
+++ script/clock_checked.js
@@ -1,12 +1,17 @@
 window.onload = function () {
   
-    var seconds = 00; 
-    var tens = 00; 
-    var appendTens = document.getElementById("tens")
-    var appendSeconds = document.getElementById("seconds")
+    var seconds = 0; 
+    var tens = 0; 
+    var hours = 0;
+    var minutes = 0;
+    var appendTens = document.getElementById("tens");
+    var appendSeconds = document.getElementById("seconds");
+    var appendHours = document.getElementById("hours");
+    var appendMinutes = document.getElementById("minutes");
     var buttonStart = document.getElementById('button-start');
     var buttonStop = document.getElementById('button-stop');
     var buttonReset = document.getElementById('button-reset');
+    var buttonCountdown = document.getElementById('button-countdown');
     var Interval ;
   
     buttonStart.onclick = function() {
@@ -24,8 +29,12 @@ window.onload = function () {
        clearInterval(Interval);
       tens = "00";
       seconds = "00";
+      hours = "00";
+      minutes = "00";
       appendTens.innerHTML = tens;
       appendSeconds.innerHTML = seconds;
+      appendHours.innerHTML = hours;
+      appendMinutes.innerHTML = minutes;
     }
     
      
@@ -53,7 +62,40 @@ window.onload = function () {
       if (seconds > 9){
         appendSeconds.innerHTML = seconds;
       }
-    
+
+      if (seconds>59)
+      {
+        console.log("minute");
+        minutes++;
+        appendMinutes.innerHTML = "0" + minutes;
+        seconds = 0;
+        appendSeconds.innerHTML = "0" + 0;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+      }
+      
+      if(minutes >9)
+      {
+        appendMinutes.innerHTML = minutes;
+      }
+      
+      if (minutes>59)
+      {
+        console.log("hours");
+        hours++;
+        appendHours.innerHTML = "0" + hours;
+        minutes = 0;
+        appendMinutes.innerHTML = "0" + 0;
+        seconds = 0;
+        appendSeconds.innerHTML = "0" + 0;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+      }
+      
+      if(hours >9)
+      {
+        appendHours.innerHTML = hours;
+      }
     }
     
   
